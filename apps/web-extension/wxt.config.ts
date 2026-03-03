6eba967e5fca60be4317bb20eca4501970d9c77d
datguyducky <36929645+datguyducky@users.noreply.github.com>
2026-03-03 21:17:28 +0100

    chore: clean up wxt.config.ts by removing unused `path` import and redundant `vite` configuration

--- apps/web-extension/wxt.config.ts
+++ apps/web-extension/wxt.config.ts
@@ -1,14 +1,9 @@
-import path from 'node:path';
 import { defineConfig } from 'wxt';
 
 export default defineConfig({
 	srcDir: 'src',
 	outDir: 'dist',
 	modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
-
-	vite: () => ({
-		envDir: path.resolve(__dirname, '../../'),
-	}),
 
 	dev: {
 		server: {
